f26d3454d81384beae3749bbc74d422bfde5fc16
Stefano Corra <stefano.corra@hotmail.com>
2017-06-12 23:57:11 -0400

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {AlertModule} from 'ng2-bootstrap/ng2-bootstrap'
+import {AlertModule} from 'ng2-bootstrap/ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent} from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
